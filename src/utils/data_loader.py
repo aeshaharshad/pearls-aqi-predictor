0f1d89dd3aa5ab3d1aa93b4b6810fe2505aca43a
aeshaharshad <aeshaharshad65@gmail.com>
2026-02-16 00:26:59 +0500

    Pass MongoDB and DagsHub env vars to workflow

--- src/utils/data_loader.py
+++ src/utils/data_loader.py
@@ -1,17 +1,16 @@
 import os
 import pandas as pd
 from pymongo import MongoClient
-from dotenv import load_dotenv
 
+MONGODB_URI = os.getenv("MONGODB_URI")
+DB_NAME = os.getenv("MONGODB_DB", "aqi_db")
+COLLECTION = os.getenv("MONGODB_COLLECTION", "aqi_features")
+
+if not MONGODB_URI:
+    raise ValueError("MONGODB_URI is not set")
+
+client = MongoClient(MONGODB_URI)
+collection = client[DB_NAME][COLLECTION]
 
 def load_data():
-    load_dotenv()
-
-    MONGO_URI = os.getenv("MONGODB_URI")
-    DB_NAME = os.getenv("MONGODB_DB")
-    COLLECTION = os.getenv("MONGODB_COLLECTION")
-
-    client = MongoClient(MONGO_URI)
-    df = pd.DataFrame(list(client[DB_NAME][COLLECTION].find()))
-
-    return df
+    return pd.DataFrame(list(collection.find()))
